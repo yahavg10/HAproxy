b7f8d7f94ac7786e80fc1a68f4ee64cc8b7e9772
yahavg10 <yahavg10@gmail.com>
2024-07-04 11:04:39 +0300

    updated service

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,9 +1,9 @@
 import yaml
 
-from utils.variables_utils import ConfigModelsOptions
+from models.app_model import AppConfig
 
 
-def load_configuration(config_model: ConfigModelsOptions, path_to_load_from: str):
+def load_configuration(config_model: AppConfig, path_to_load_from: str):
     with open(file=path_to_load_from) as config_file:
         config_data = yaml.safe_load(config_file)
-        return config_model.from_dict(config_data, config_model)
+        return config_model.from_dict(config_data, AppConfig)
--- utils/variables_utils.py
+++
@@ -1,5 +0,0 @@
-from typing import Union, Type
-
-from models.app_model import AppConfig
-
-ConfigModelsOptions = Union[Type[AppConfig]]
